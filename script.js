939b9f236d1225b6cee91057e1e6bcae004f963c
Felipe Esaki Roman <120192433+FelipeEsaki@users.noreply.github.com>
2023-08-18 20:21:28 -0300

    Commit 1

---
+++ script.js
@@ -0,0 +1,36 @@
+let image1 = document.getElementById ('image1')
+let image2 = document.getElementById ('image2')
+let slide1 = document.getElementById ('slide1')
+let slide2 = document.getElementById ('slide2')
+let menu = document.getElementById ('menu')
+let count = 1
+
+document.getElementById ('radio1').checked = true;
+
+setInterval ( function() {
+    nextImage ();
+}, 4000);
+
+function nextImage () {
+    count++;
+    if (count > 2) {count=1};
+
+    document.getElementById ('radio' + count).checked = true;
+}
+
+window.reveal = ScrollReveal ({ reset: true});
+
+reveal.reveal ('.solarSystem', {
+duration: 1800,
+scale: .1
+});
+
+function clickMenu () {
+    if (menu.style.display == 'none') {
+        menu.style.display = 'block'
+    }
+    else {
+        menu.style.display = 'none'
+    }
+};
+
